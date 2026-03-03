5a3a74e92ef7c0bd36c345e22993c8d917a4ae5e
infobrand-dev <93970661+infobrand-dev@users.noreply.github.com>
2026-03-03 21:27:42 +0700

    chore: remove temporary session diagnostic endpoints

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\InstallController;
 use App\Http\Controllers\ModuleController;
-use App\Http\Controllers\SessionDiagnosticController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,8 +48,5 @@ Route::middleware(['auth', 'role:Super-admin'])->group(function () {
     Route::post('/modules/{slug}/activate', [ModuleController::class, 'activate'])->name('modules.activate');
     Route::post('/modules/{slug}/deactivate', [ModuleController::class, 'deactivate'])->name('modules.deactivate');
 });
-
-Route::get('/_debug/session', [SessionDiagnosticController::class, 'show'])->name('debug.session.show');
-Route::post('/_debug/session/post', [SessionDiagnosticController::class, 'post'])->name('debug.session.post');
 
 require __DIR__ . '/auth.php';

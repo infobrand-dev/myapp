7aad3dc8c4571b74a9fc7540bf5bcbe747919cd5
infobrand-dev <93970661+infobrand-dev@users.noreply.github.com>
2026-04-04 19:26:12 +0700

    update ResolveTenantContext.php

--- app/Http/Middleware/ResolveTenantContext.php
+++ app/Http/Middleware/ResolveTenantContext.php
@@ -129,6 +129,7 @@ class ResolveTenantContext
             || $request->is('meetra')
             || $request->is('omnichannel')
             || $request->is('accounting')
+            || $request->is('mulai-digital')
             || $request->is('tentang-kami')
             || $request->is('affiliate-program')
             || $request->is('onboarding')
